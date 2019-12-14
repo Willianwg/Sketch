47dabc3546479b5d1c990a7745a6f2b15fa87d5f
willian <willian.wg70@gmail.com>
2019-12-13 23:53:25 -0300

    feat: add function to register push notification token

--- mobile/src/pages/Inbox.js
+++ mobile/src/pages/Inbox.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useMemo } from "react"; 
 import { AsyncStorage, KeyboardAvoidingView, View, StyleSheet, Text, FlatList, TextInput, TouchableOpacity } from "react-native"; 
 import socketio from "socket.io-client";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
+import api from "../services/api";
 
 export default function Inbox({ navigation }){
 	const [ messages, setMessages ] = useState([]);
@@ -31,6 +34,11 @@ export default function Inbox({ navigation }){
 		
 	}, [ ] ); 
 	
+	async function registerToken(){
+		const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+		const token = await Notifications.getExpoPushTokenAsync();
+		const response = await api.post("/users/push_token", { token }, { headers:{ user:user_id }});
+	};
 	function sendMessage(){
 		if(! newMessage.trim() ) return;
 		const message ={ to:talkingTo._id, message:newMessage.trim(), from:user_id };
